175a828f2ebc7baf0df536eed82726492402bd28
Rohithsingh123 <144004766+Rohithsingh123@users.noreply.github.com>
2023-10-16 00:58:34 +0530

    first
    first commit

---
+++ demo.js
@@ -0,0 +1,20 @@
+const p = document.getElementById('count') 
+let c = 0; 
+function increment ()
+{
+    c = c + 1; 
+    p.innerHTML = c;
+}
+
+function decrement ()
+{
+    if(c>0)
+    c = c - 1;
+    p.innerHTML = c;
+}
+
+function reset ()
+{
+    c = 0;
+    p.innerHTML = c;
+}
